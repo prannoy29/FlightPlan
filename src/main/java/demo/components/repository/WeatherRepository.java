45f4a79b3a6bda57b6d7748f2f8369d068f75509
Srinivas Kotla <srinivas.kotla@zemosolabs.com>
2017-09-21 18:46:36 +0530

    Modifications done to weather APIs to support any date

--- src/main/java/demo/components/repository/WeatherRepository.java
+++ src/main/java/demo/components/repository/WeatherRepository.java
@@ -1,9 +1,11 @@
 package demo.components.repository;
 
 import com.google.gson.Gson;
+import demo.components.domain.GeoPoint;
 import demo.components.domain.Weather;
 import org.apache.lucene.search.join.ScoreMode;
 import org.apache.lucene.spatial3d.geom.GeoDistance;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
@@ -16,6 +18,9 @@ import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.sort.SortBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -38,22 +43,43 @@ public class WeatherRepository {
         }catch (Exception ex){}
     }
 
-    public Weather getData(){
-        //GeoDistanceQueryBuilder qb = QueryBuilders.geoDistanceQuery("location").point(48.08,-103.69).distance(1,DistanceUnit.DEFAULT);
+    public GeoPoint getPoint(Double lat,Double lon){
+        QueryBuilder qb = geoDistanceQuery("location").point(lat, lon).distance(10, DistanceUnit.KILOMETERS);
+        SortBuilder sortBuilder = SortBuilders.geoDistanceSort("location",lat,lon).order(SortOrder.ASC);
+        SearchResponse searchResponse = client.prepareSearch("partner_data_us_west")
+                                            .setTypes("weather")
+                                            .setQuery(qb)
+                                            .addSort(sortBuilder)
+                                            .execute()
+                                            .actionGet();
+        SearchHits hits = searchResponse.getHits();
+        if(hits.totalHits() == 0){
+            throw new NullPointerException("No weather data available at the location");
+        }
+        else {
+            SearchHit hit = hits.getAt(0);
+            JSONObject object = new JSONObject(hit.getSource());
+            JSONObject object1 = object.getJSONObject("location");
+            GeoPoint geoPoint = new GeoPoint(object1.getDouble("lat"),object1.getDouble("lon"));
+            return geoPoint;
+        }
+
+    }
+
+    public Weather getData(String id){
         //QueryBuilder qb = geoBoundingBoxQuery("location").setCorners(48.08, -103.69, 48.07, -103.70);
-        QueryBuilder qb = geoDistanceQuery("location").point(48.08, -103.69).distance(1, DistanceUnit.KILOMETERS);
-        //QueryBuilder qb = QueryBuilders.matchQuery("id","103_69W_48_12N_2017_07_16T12");
+        /*QueryBuilder qb = QueryBuilders.matchQuery("id",id);
         SearchResponse searchResponse = client.prepareSearch("partner_data_us_west")
                                               .setTypes("weather")
                                               .setQuery(qb)
                                               .execute()
                                               .actionGet();
         SearchHits hits = searchResponse.getHits();
-        SearchHit hit = hits.getAt(1);
-        JSONObject object = new JSONObject(hit.getSource());
+        SearchHit hit = hits.getAt(0);*/
+        GetResponse response = client.prepareGet("partner_data_us_west","weather",id).get();
+        JSONObject object = new JSONObject(response.getSource());
         Weather weather = new Gson().fromJson(object.toString(),Weather.class);
         return weather;
     }
-
 
 }
