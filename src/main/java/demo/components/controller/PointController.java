23dffe5835cead64a6471fb83c27a3cc50236b64
prannoy29 <prannoysarkar1993@gmail.com>
2017-09-25 15:32:07 +0530

    Created API that returns path where should not be flown

--- src/main/java/demo/components/controller/PointController.java
+++ src/main/java/demo/components/controller/PointController.java
@@ -1,10 +1,16 @@
 package demo.components.controller;
 
 import demo.components.dao.PointDao;
+import demo.components.domain.GeoJsonLineString;
 import demo.components.domain.GeoJsonPoint;
 
+import demo.components.domain.GeoPoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PointController {
@@ -13,11 +19,16 @@ public class PointController {
 
     @RequestMapping(value = "point/getElevation",method = RequestMethod.POST)
     public double getElevation(@RequestBody GeoJsonPoint geoJsonPoint){
-
-        System.out.println(geoJsonPoint.toString());
         String point = geoJsonPoint.toString();
         double elevation = pointDao.getElevation(point);
         return elevation;
+    }
+
+    @RequestMapping(value = "/getRestrictPath",method = RequestMethod.POST)
+    public List<GeoPoint> getRestrictPath(@RequestBody GeoJsonLineString geoJsonLineString){
+        String queryLine = geoJsonLineString.toString();
+        List<GeoPoint> pointsLineList= pointDao.getRestrictedPathPoints(queryLine,3,2);
+        return pointsLineList;
     }
 
 }
